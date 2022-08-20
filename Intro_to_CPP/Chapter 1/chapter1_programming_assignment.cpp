14899cbce2e66d9ddd2f8f6515a2770e7341daac
DrWizardDrunkenstein54 <48422595+DrWizardDrunkenstein54@users.noreply.github.com>
2022-08-20 02:04:14 -0700

    chapter 1 programming homework
    P1.7

---
+++ Intro_to_CPP/Chapter 1/chapter1_programming_assignment.cpp
@@ -0,0 +1,15 @@
+// Brian Da Cruz
+// 8/19/22
+// lab 1, P1.7
+// This program prints my name surrounded by a box made up of dash characters
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    cout << "-----------" << endl << "|  Brian  |" << endl << "-----------" << endl;
+    
+    return 0;
+}

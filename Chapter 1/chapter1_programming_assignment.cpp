d8850aa0dcf9e5985b1d18f12fc9734160b17871
DrWizardDrunkenstein54 <48422595+DrWizardDrunkenstein54@users.noreply.github.com>
2022-08-28 17:54:45 -0700

    comment fix

--- Chapter 1/chapter1_programming_assignment.cpp
+++ Chapter 1/chapter1_programming_assignment.cpp
@@ -1,7 +1,7 @@
 // Brian Da Cruz
 // 8/19/22
 // lab 1, P1.7
-// This program prints my name surrounded by a box made up of dash characters
+// This program prints my name surrounded by a box made up of dash and absolute value characters
 
 
 #include <iostream>

97d8ebe0c045ee198872d7aaedb761e201d9d5bc
DrWizardDrunkenstein54 <48422595+DrWizardDrunkenstein54@users.noreply.github.com>
2022-09-21 01:18:07 -0700

    if worksheet

--- Chapter 3/all_the_same.cpp
+++ Chapter 3/all_the_same.cpp
@@ -1,36 +1,36 @@
-// Brian Da Cruz
-// 9/15/22
-// P3.6
-// This program prints either "all the same", "all different", or "neither", depending on if three numbers are the same.
-
-#include <iostream>
-using namespace std;
-
-int main()
-{
-    
-    // Read in input numbers
-    double num1, num2, num3;
-    cin >> num1 >> num2 >> num3;
-
-    
-    // Check if all the numbers are the same before checking if only 2 of them are the same
-    if (num1 == num2 && num2 == num3)
-    {
-        cout << "all the same" << endl;
-    }
-    
-    // Check if all the numbers are different
-    else if (num1 != num2 && num2 != num3 && num3 != num1)
-    {
-        cout << "all different" << endl;
-    }
-    
-    // Else condition: none of the numbets are the same
-    else
-    {
-        cout << "neither" << endl;
-    }
-
-    return 0;
-}
+//// Brian Da Cruz
+//// 9/15/22
+//// P3.6
+//// This program prints either "all the same", "all different", or "neither", depending on if three numbers are the same.
+//
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//
+//    // Read in input numbers
+//    double num1, num2, num3;
+//    cin >> num1 >> num2 >> num3;
+//
+//
+//    // Check if all the numbers are the same before checking if only 2 of them are the same
+//    if (num1 == num2 && num2 == num3)
+//    {
+//        cout << "all the same" << endl;
+//    }
+//
+//    // Check if all the numbers are different
+//    else if (num1 != num2 && num2 != num3 && num3 != num1)
+//    {
+//        cout << "all different" << endl;
+//    }
+//
+//    // Else condition: none of the numbets are the same
+//    else
+//    {
+//        cout << "neither" << endl;
+//    }
+//
+//    return 0;
+//}
